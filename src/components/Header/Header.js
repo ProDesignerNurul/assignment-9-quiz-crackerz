1f9dbbbb6433a705b23191112b2d1b0df13eae36
prodesignernurul <maxnewsbd@gmail.com>
2022-10-10 21:20:14 +0600

    complate navBar with Logo Text

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <Link to='/quizes'>Quizes</Link>
-            <Link to='/statistic'>Statistic</Link>
-            <Link to='/blogs'>Blogs</Link>
+        <div className='header'>
+            <div className='logo'>
+                <h2>Quiz Rider</h2>
+            </div>
+            <div className='nav-bar'>
+                <Link to='/quizes'>Quizes</Link>
+                <Link to='/statistic'>Statistic</Link>
+                <Link to='/blogs'>Blogs</Link>
+            </div>
         </div>
     );
 };
