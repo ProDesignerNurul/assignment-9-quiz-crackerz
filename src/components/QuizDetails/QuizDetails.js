ab56d03871e6981072ae28f94c0584c7abc290cb
ProDesignerNurul <maxnewsbd@gmail.com>
2022-10-11 02:19:04 +0600

    question page added

--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import QuizQuestions from '../QuizQuestions/QuizQuestions';
+import './QuizDetails.css';
 
 const QuizDetails = () => {
     const quizDetails = useLoaderData();
+    // const {id, } = quizDetails.data.questions
     // const {question} = quizDetails.data.questions;
-    // console.log(quizDetails.data.questions)
+    console.log(quizDetails.data.questions[1].options)
     return (
         <div>
-            
+            <h2> Question : </h2>
+            <div className='quiz-details'>
+                {
+                    quizDetails.data.questions.map(quizQuestion => <QuizQuestions
+                        key={quizDetails.data.questions.id}
+                        quizQuestion={quizQuestion}
+                    ></QuizQuestions>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/QuizQuestions/QuizQuestions.js
+++ src/components/QuizQuestions/QuizQuestions.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './QuizQuestions.css';
 
-const QuizQuestions = () => {
+const QuizQuestions = ({quizQuestion}) => {
+    console.log(quizQuestion)
+    const {question, options} = quizQuestion;
     return (
-        <div>
-            <h2>Questions</h2>
+        <div className='question-section'>
+            <h2>Questions : {question}</h2>
+            <h3 className='answore'>Answore : {options}</h3>
         </div>
     );
 };
