971ddb6b88537b9d63a7fd70dcb0f8f5e10e9bf3
prodesignernurul <maxnewsbd@gmail.com>
2022-10-10 21:30:59 +0600

    complate another some work

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './Main/Main';
 import Quizes from './components/Quizes/Quizes';
 import Statistic from './components/Statistic/Statistic';
+import Blogs from './components/Blogs/Blogs';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '/', element: <Main></Main>, children: [
         {
-          path: '/', element: <Quizes></Quizes>
+          path: '/', 
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <Quizes></Quizes>
         },
         {
-          path: '/quizes', element: <Quizes></Quizes>
+          path: '/quizes', 
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <Quizes></Quizes>
         },
         {
           path: '/statistic', element: <Statistic></Statistic>
+        },
+        {
+          path: '/blogs', element: <Blogs></Blogs>
         }
       ]
     }
--- src/components/Quizes/Quizes.js
+++ src/components/Quizes/Quizes.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useRouteLoaderData } from 'react-router-dom';
+import Quiz from '../Quiz/Quiz';
 
 const Quizes = () => {
+    const quizes = useRouteLoaderData();
     return (
         <div>
             <h2>This is Quizes Page</h2>
+            
         </div>
     );
 };
