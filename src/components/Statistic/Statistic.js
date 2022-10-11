5d128b6339d6f128849753b6042a2d874ef25ae6
prodesignernurul <maxnewsbd@gmail.com>
2022-10-11 20:12:09 +0600

    complate chart section

--- src/components/Statistic/Statistic.js
+++ src/components/Statistic/Statistic.js
@@ -1,26 +1,34 @@
 import axios from 'axios';
+import './Statistic.css';
 import React, { useEffect, useState } from 'react';
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
+
 
 const Statistic = () => {
-const [quizes, setQuizes] = useState([]);
-useEffect(() => {
-    axios.get('https://openapi.programming-hero.com/api/quiz')
-    .then(data => {
-        const quizLoaded = data.data.data;
-        const quizData = quizLoaded.map( quiz => {
-            const singleQuiz = {
-                name: quiz.name,
-                total: quiz.total,
-            }
-            return singleQuiz;
-        })
-        console.log(quizLoaded)
-        setQuizes(quizData);
-    })
-}, [])
+    const [quizes, setQuizes] = useState([]);
+    useEffect(() => {
+        axios.get('https://openapi.programming-hero.com/api/quiz')
+            .then(data => {
+                const quizLoaded = data.data.data;
+                const quizData = quizLoaded.map(quiz => {
+                    const singleQuiz = {
+                        name: quiz.name,
+                        total: quiz.total,
+                    }
+                    return singleQuiz;
+                })
+                // console.log(quizLoaded)
+                setQuizes(quizData);
+            })
+    }, [])
     return (
-        <div>
-            <h2>this is Statistic page</h2>
+        <div className='chart'>
+            <BarChart width={500} height={300} data={quizes}>
+                <Bar dataKey="total" fill="#8884d8" />
+                <Tooltip></Tooltip>
+                <XAxis datakey="name"></XAxis>
+                <YAxis></YAxis>
+            </BarChart>
         </div>
     );
 };
